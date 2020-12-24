b1ffd158d3f7f7f2aad37571ddf9088eb194499a
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-12-24 14:11:55 -0500

    Changed frontend and also added models

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Board
+from .models import *
 # Register your models here.
 
 class BoardAdmin(admin.ModelAdmin):
     list_display = ('dispatch', 'load_number', 'pickup_number', 'driver', 'phone_number', 'origin', 'delivery', 'mile', 'rate', 'broker')
+
+
 
 admin.site.register(Board, BoardAdmin)
